325eaff0edaaca2de41c1d3d02b15e81f699da1e
zahra <zAHRAkrd7@gamil.com>
2024-01-06 23:43:58 -0800

    Update project & read me

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@ const API_KEY = "90ac90db30ba348d6468d8042cfbad9d";
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
-const forecastContainer = document.getElementById("forecast  ");
+const forecastContainer = document.getElementById("forecast");
 const locationIcon =document.getElementById("location");
+// const modalButton =document.getElementById("modal-button");
+
 const DAYS= [
     "sunday",
     "Monday",
@@ -18,6 +20,7 @@ const DAYS= [
     "Friday",
     "Saturday",
 ];
+// import { removeModal, showModal } from "./function/modal.js";
 
 
 const getCurrentWeatherByName = async city => {
@@ -42,13 +45,13 @@ const getForcastWeatherByName = async (city) => {
 
 
 const renderCurrentWeather = (data) => {
-    
+     if (!data) return;
     const weatherJSx= `
     <h1>${data.name} , ${data.sys.country}</h1>
     <div id="main">
     <img  alt="weather icon" src="http://openweathermap.org/img/w/
     ${data.weather[0].icon}.png" />
-
+    
     <span>${data.weather[0].main}</span>
     <p>${Math.round(data.main.temp) + "°C"}</p>
     </div> 
@@ -58,6 +61,7 @@ const renderCurrentWeather = (data) => {
 
     </div>
     `;
+    
     weatherContainer.innerHTML = weatherJSx;
 };
   
@@ -66,13 +70,13 @@ const renderCurrentWeather = (data) => {
     };
 
     const renderForecastWeather = (data) =>{
-        forecastContainer.innerHTML ="";
-       data = data.list.filter((obj) => obj.dt_txt.endWish("12:00:00"));
+        forecastContainer.innerHTML =" ";
+       data = data.list.filter((obj) => obj.dt_txt.endsWith("12:00:00"));
         data.forEach((i) => { 
             const forecastJsx =` 
             <div>
             <img  alt="weather icon" src="http://openweathermap.org/img/w/
-                   ${i.weather[0].icon}.png" />
+                   ${i.weather[0].icon}.png"/>
                    <h3>${getWeekDay(i.dt)}</h3>
                    <p>${Math.round(i.main.temp) + "°C" }  </p>
                    <span>${i.weather[0].main}</span>
@@ -84,8 +88,8 @@ const renderCurrentWeather = (data) => {
         });
 
     };
-    const getForecastWeatherByCoordinates = async (city) => {
-        const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric `;
+    const getForecastWeatherByCoordinates = async (lat,lon) => {
+    const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric `
         const response = await fetch(url);
         const json = await response.json();
         return json;
@@ -95,7 +99,8 @@ const searchHandler = async () => {
     const cityName = searchInput.value;
 
     if(!cityName){
-        alert("please enter name of city :) ")
+    alert("please enter name of city :) ")
+    return;
     }
 
    const currentData = await getCurrentWeatherByName(cityName);
@@ -121,10 +126,11 @@ const locatonHandler = () => {
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(positionCallback , errorCallback )
     }else{
-        alert("your browser does not support geolocatin")
+    alert("your browser does not support geolocatin")
     }
 };
 
 
 searchButton.addEventListener("click" , searchHandler);
 locationIcon.addEventListener("click" , locatonHandler);
+// modalButton.addEventListener( "click" , removeModal);
